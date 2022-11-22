5605cba42768ab977b037184a3a1b7a0fd33d1e5
Jaime Navarrete <jaimenava123@outlook.com>
2022-11-22 12:10:23 -0600

    Add reservation events listing page
    - This adds the reservation templates.
    - Creates the index view and adds its url to the project.

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import ReservationEvent
 
 # Create your views here.
+def index(request) :
+    reservation_events = ReservationEvent.objects.all()
+    
+    return render(request, 'reservations/index.html', {
+        'reservation_events': reservation_events
+    })
