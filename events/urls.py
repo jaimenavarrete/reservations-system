7f4eea0a3d3eb573a0d3b372c96172c400a34551
Jaime Navarrete <jaimenava123@outlook.com>
2022-12-02 17:35:38 -0600

    Add reservation event creation feature
    - Adds event creation form.
    - Implements and configure messages framework.
    - Adds url names.

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "events"
+
 urlpatterns = [
-    path('', views.index),
-    path('create_event/', views.create_event)
+    path('', views.index, name='index'),
+    path('create_event/', views.create_event, name='create_event')
 ]
