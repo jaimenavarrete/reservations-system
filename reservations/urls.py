02476dadfa1bd944beb39d58c2871723f6989e8c
Jaime Navarrete <jaimenava123@outlook.com>
2022-12-10 13:35:50 -0600

    Add reservation creation feature
    - Gets event info by link and add it on reservations page.

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'reservations'
 
 urlpatterns = [
-    path('view_event/', views.view_event, name='view_event'),
+    path('view_event/<str:event_link>', views.view_event, name='view_event'),
     path('get_reservation_times/', views.get_reservation_times, name='get_reservation_times')
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -3,12 +3,29 @@ from django.http import JsonResponse
 from django.contrib import messages
 from datetime import datetime, timedelta
 from .forms import ReservationForm
+from events.models import ReservationEvent
 
 # Create your views here.
-def view_event(request) :
-    form = ReservationForm()
+def view_event(request, event_link) :
+    searched_event = ReservationEvent.objects.filter(event_link=event_link)
+    
+    if not searched_event.exists() :
+        messages.error(request, "The event does not exist!")
+        return redirect('events:index')
+    
+    event = searched_event[0]
+    
+    form = ReservationForm(request.POST or None)
+    
+    if request.method == 'POST' :
+        if form.is_valid() :
+            form.save()
+
+            messages.success(request, 'The reservation was made successfully')
+            return redirect('reservations:view_event', event_link=event_link)
     
     return render(request, 'reservations/view_event.html', {
+        'event': event,
         'form': form
     })
 
